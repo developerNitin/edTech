ec5724ca710f5c9d616ecd113f296355a1ba56b4
nitin <nitin.birdi@icloud.com>
2022-10-12 02:04:53 +0530

    commit

--- src/UI/navbar/navbar.js
+++ src/UI/navbar/navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useHref } from "react-router-dom";
 import { useState } from "react";
 import {
   RiDashboardFill,
@@ -9,31 +9,31 @@ import {
   RiArrowDropRightLine,
 } from "react-icons/ri";
 
-export default function Navbar() {
-  const navlinks = [
-    {
-      name: "Dashboard",
-      link: "/dashboard",
-      icon: <RiDashboardFill className="h-[22px] w-[22px] mr-[10px]" />,
-    },
-    {
-      name: "Courses",
-      link: "/courses",
-      icon: <RiVideoFill className="h-[22px] w-[22px] mr-[10px]" />,
-    },
-    {
-      name: "Practice",
-      link: "/practice",
-      icon: <RiBook2Fill className="h-[22px] w-[22px] mr-[10px]" />,
-    },
-    {
-      name: "Chat",
-      link: "/chat",
-      icon: <RiChat1Fill className="h-[22px] w-[22px] mr-[10px]" />,
-    },
-  ];
+const navlinks = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+    icon: <RiDashboardFill className="h-[22px] w-[22px] mr-[10px]" />,
+  },
+  {
+    name: "Courses",
+    href: "/courses",
+    icon: <RiVideoFill className="h-[22px] w-[22px] mr-[10px]" />,
+  },
+  {
+    name: "Practice",
+    href: "/practice",
+    icon: <RiBook2Fill className="h-[22px] w-[22px] mr-[10px]" />,
+  },
+  {
+    name: "Chat",
+    href: "/chat",
+    icon: <RiChat1Fill className="h-[22px] w-[22px] mr-[10px]" />,
+  },
+];
 
-  const [active, setActive] = useState(0);
+export default function Navbar() {
+  let path = useHref();
   const [miniNav, setMiniNav] = useState(false);
 
   return (
@@ -48,15 +48,15 @@ export default function Navbar() {
         }`}
       >
         <figure className="text-[22px]">{miniNav ? "EdTech" : "Ed"}</figure>
-        <div className="flex justify-end text-[30px] mt-[35px]">
+        <div className="flex justify-end text-[30px] mt-[45px]">
           <button
-            className={` bg-black rounded-[30px] p-[3px] cursor-pointer relative z-[100] ${
+            className={`bg-black rounded-[30px] p-[3px] cursor-pointer relative z-[100] ${
               miniNav ? "mr-[-15px]" : "mr-[-21px]"
             } `}
           >
             <RiArrowDropRightLine
               onClick={() => setMiniNav(!miniNav)}
-              className={`fill-white w-[22px] h-[22px] ${
+              className={`transition ease-linear delay-[.2s] fill-white w-[22px] h-[22px] ${
                 miniNav && "rotate-180"
               }`}
             />
@@ -68,14 +68,15 @@ export default function Navbar() {
           {navlinks.map((i, idx) => (
             <li key={idx}>
               <Link
-                className={`transition-animation flex h-[46px]  items-center font-medium text-[18px] ${
+                className={`transition-animation flex h-[50px]  items-center font-medium text-[18px] ${
                   miniNav ? "pl-[70px]" : "pl-[20px] pr-[6px]"
                 }`}
-                to={i.link}
+                to={i.href}
                 style={
-                  active === idx ? { borderRight: "3px solid black" } : { borderRight: "3px solid white" }
+                  path === i.href
+                    ? { borderRight: "3px solid black" }
+                    : { borderRight: "3px solid white" }
                 }
-                onClick={() => setActive(idx)}
               >
                 <figure>{i.icon}</figure>
                 {miniNav && <span>{i.name}</span>}
